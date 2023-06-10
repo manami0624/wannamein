e30cf30ca14dba4a9aeecdde35e12357f325f1a0
manami0624 <manana624@yahoo.co.jp>
2023-06-10 12:10:12 +0900

    カテゴリーのプルダウン実装

--- app/controllers/dialogs_controller.rb
+++ app/controllers/dialogs_controller.rb
@@ -4,5 +4,21 @@ class DialogsController < ApplicationController
   end
 
   def new
+    @dialog = Dialog.new
+  end
+
+  def create
+    @dialog = Dialog.new(dialog_params)
+    if @dialog.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:dialog).permit(:language,:translation,:category_id)
   end
 end
