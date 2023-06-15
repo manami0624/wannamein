f261bf1add1f3e8df26a3729fe80147cd24234c9
manami0624 <manana624@yahoo.co.jp>
2023-06-15 15:59:32 +0900

    マイページ実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:"dialogs#index"
+  resources :users, only: :show
   resources :dialogs do
     collection do
       get 'search'
     end
   end
+  get '/users/:id/dialogs', to: 'users#dialogs', as: 'user_dialogs'
 end
